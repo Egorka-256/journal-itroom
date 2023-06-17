993b0aba6a46e4be9fe2396fd1b828dac74f1f67
Egorka-256 <egorkakasdfgh0@gmail.com>
2023-06-17 16:58:54 +0500

    journal v1.2

--- frontend/student/js/main.js
+++ frontend/student/js/main.js
@@ -1,16 +1,17 @@
 const color = document.getElementById("color");
-let theme = document.getElementById("theme");
 
 color.onclick = function () {
-    if (theme.getAttribute("href") == "../black.css") {
-        theme.href = "../white.css";
+    let theme = document.getElementById("theme");
+    if (theme.getAttribute("href") == "../css/black.css") {
+        theme.href = "../css/white.css";
     } else {
-        theme.href = "../black.css";
+        theme.href = "../css/black.css";
     }
 
-    if (color.innerHTML == "<h2>☀️</h2>") {
-        color.innerHTML = "<h2>🌙</h2>"
-    } else {
+    if (color.innerHTML == "<h2>🌙</h2>") {
         color.innerHTML = "<h2>☀️</h2>"
+    } else {
+        color.innerHTML = "<h2>🌙</h2>"
     }
 }
+
